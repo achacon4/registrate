c7017ebeffd1c5178878616d4d78ff68d9aff825
Johnatan Pinto <jfpinto6@misena.edu.co>
2017-03-18 17:32:44 -0500

    Controlador RegistroAsistente.adicionar

---
+++ controlador/RegistroAsistente.adicionar.php
@@ -0,0 +1,46 @@
+<?php
+namespace controlador;
+require_once '../entidad/RegistroAsistente.php';
+require_once '../modelo/RegistroAsistente.php';
+
+$retorno = array("exito"=>1,"mensaje"=>"","data"=>"");
+    try
+    {
+        
+        $idAsistenteEvento = $_POST['idAsistenteEvento'];
+        $idEventoFK = $_POST['txtEvento'];
+        $nombre= $_POST['txtNombre'];
+        $apaterno = $_POST['txtApaterno'];
+        $amaterno= $_POST['txtAmaterno'];
+        $tipoDocumento = $_POST['selTipoDocumento'];
+        $numeroDocumento = $_POST['txtNumeroDocumento'];
+        $email = $_POST['txtEmail'];
+        $telefono = $_POST['txtTelefono'];
+        
+
+        $clienteE = new \entidad\RegistroAsistente();
+        $clienteE->setIdAsistenteEvento($idAsistenteEvento);
+        $clienteE->setIdEventoFK($idEventoFK);
+        $clienteE->setNombre($nombre);
+        $clienteE->setApaterno($apaterno);
+        $clienteE->setAmaterno($amaterno);
+        $clienteE->setTipoDocumento($tipoDocumento);
+        $clienteE->setNumeroDocumento($numeroDocumento);
+        $clienteE->setEmail($email);
+        $clienteE->setTelefono($telefono);
+
+
+
+        $clienteM = new \modelo\RegistroAsistente(clienteE);
+        $clienteM->adicionar();
+
+        $retorno['mensaje'] = 'Se adicionó correctamente el asistente';
+        
+
+    }catch(Exception $error)
+    {
+        $retorno['exito'] = 0;
+        $retorno['mensaje']=$error->getMessage();
+        
+    }
+   echo json_encode($retorno);
