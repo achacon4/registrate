e63e036b84beca9e950ef9b48b2753b488b723d2
Unknown <jpalvarez60@misena.edu.co>
2017-03-18 22:07:37 -0500

    Controladores de Lugar (adicionar, eliminar, modificar,consultar ) con sus log

---
+++ controlador/Lugar.modificar.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../modelo/Lugar.php'; 
+
+$retorno= array("exito"=>1,"mensaje"=>"");
+
+try {
+       $nombre = filter_input(INPUT_POST,'txtNombre');
+       $disponibilidad = filter_input(INPUT_POST, 'selDisponibilidad');
+       $descripcion = filter_input (INPUT_POST, 'txtDescripcion');
+       $presupuesto = filter_input (INPUT_POST, 'txtPresupuesto');
+       $cantidadPersonas = filter_input (INPUT_POST, 'txtCantidadPersonas');
+       
+       $entidadE= new \entidad\Lugar();
+       $entidadE-> setNombre ($nombre); 
+       $entidadE-> setDisponibilidad ($disponibilidad);
+       $entidadE-> setDescripcion ($descripcion);
+       $entidadE-> setPresupuesto ($presupuesto);
+       $entidadE-> setCantidadPersonas ($cantidadPersonas);
+       
+        $entidadM->conexion->iniciarTransaccion();
+        $entidadM->conexion->confirmarTransaccion();
+        $retorno['mensaje']='Se modifico Correctamente';
+} catch (Exception $ex) {
+    $entidadM->conexion->cancelarTransaccion();
+    $retorno['exito']=0;
+    $retorno['mensaje']=$error->getMessage();
+}
+echo json_encode($retorno);
+
