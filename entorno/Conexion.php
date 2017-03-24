d8101ed86d67aa0f337be39a326780172d620fed
Alexander Chacon Medina <achacon4@misena.edu.co>
2017-03-24 15:30:44 -0500

    .

--- entorno/Conexion.php
+++ entorno/Conexion.php
@@ -5,7 +5,7 @@ class Conexion{
     
     function __construct(){
         //$this->conn = new PDO("mysql:host=localhostooo;port=3306;dbname=clientes;charset=utf8", "root", "123");
-        $this->conn = new mysqli('localhost:3306','root','123', 'registrate', 3306);
+        $this->conn = new mysqli('10.73.188.71:3306','registrate','1234.abcD*', 'registrate', 3306);
         //$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     public function ejecutar($sentenciaSql){
