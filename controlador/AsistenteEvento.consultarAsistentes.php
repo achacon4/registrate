13e1a4a690144c50aa5893956e713af801598357
jevelez681 <jevelez681@misena.edu.co>
2017-03-26 16:10:15 -0500

    correción

--- controlador/AsistenteEvento.consultarAsistentes.php
+++ controlador/AsistenteEvento.consultarAsistentes.php
@@ -15,9 +15,9 @@ try{
     while ($fila = $eventoM->conexion->obtenerObjeto()){
         
         $retorno['data']['asistentes'][$contador]['idAsistenteEvento']= $fila->idAsistenteEvento;
-        $retorno['data']['asistentes'][$contador]['nombre']= $fila->nombre;
-        $retorno['data']['asistentes'][$contador]['apaterno']= $fila->apaterno;
-        $retorno['data']['asistentes'][$contador]['amaterno']= $fila->amaterno;
+        $retorno['data']['asistentes'][$contador]['nombre']= utf8_encode($fila->nombre);
+        $retorno['data']['asistentes'][$contador]['apaterno']= utf8_encode($fila->apaterno);
+        $retorno['data']['asistentes'][$contador]['amaterno']= utf8_encode($fila->amaterno);
         $retorno['data']['asistentes'][$contador]['tipoDocumento']= $fila->tipoDocumento;
         $retorno['data']['asistentes'][$contador]['numeroDocumento']= $fila->numeroDocumento;
         $retorno['data']['asistentes'][$contador]['telefono']= $fila->telefono;
